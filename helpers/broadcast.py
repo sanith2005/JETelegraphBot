e2c592bf48ffc7ce277881790def9c7fcd6842fc
Sanith   Bimsara <77660641+sanith2005@users.noreply.github.com>
2021-07-23 23:03:57 -0700

    Update broadcast.py

--- helpers/broadcast.py
+++ helpers/broadcast.py
@@ -5,7 +5,7 @@ import asyncio
 import datetime
 import aiofiles.os
 import traceback
-import info
+import config
 from helpers.database.access_db import db
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
